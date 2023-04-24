133a76e9b680e3fee2ab7f53e82c39e8688b148b
rakshithgr7 <122076500+rakshithgr7@users.noreply.github.com>
2023-04-24 16:57:11 +0530

    Add files via upload

---
+++ jenkins/TestCalculator.py
@@ -0,0 +1,21 @@
+import unittest
+from Calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_addition(self):
+        result = self.calc.add(3, 4)
+        self.assertEqual(result, 7)
+
+    def test_subtraction(self):
+        result = self.calc.subtract(7, 3)
+        self.assertEqual(result, 4)
+
+    def test_multiplication(self):
+        result = self.calc.multiply(2, 5)
+        self.assertEqual(result, 10)
+
+if __name__ == '__main__':
+    unittest.main()
